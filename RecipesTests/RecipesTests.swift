5fbb5a04c3097f6e9cccac19cd7a0ac3bacdccfb
Al Wold <alwold@alwold.com>
2024-01-28 14:26:06 -0700

    Add a snapshot test

--- RecipesTests/RecipesTests.swift
+++ RecipesTests/RecipesTests.swift
@@ -1,5 +1,15 @@
+import SnapshotTesting
+import SwiftUI
 import XCTest
+
 @testable import Recipes
 
 final class RecipesTests: XCTestCase {
+    func testSnapshot() {
+        let view = NavigationStack {
+            RecipesView()
+        }
+        
+        assertSnapshot(of: view, as: .image)
+    }
 }
