dfbe261ceaaeae8283476d028b0eed6ec5dfb26b
Al Wold <alwold@alwold.com>
2024-01-28 14:37:22 -0700

    Add UI test

--- RecipesUITests/RecipesUITests.swift
+++ RecipesUITests/RecipesUITests.swift
@@ -1,4 +1,30 @@
 import XCTest
 
 final class RecipesUITests: XCTestCase {
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
+        
+        app = XCUIApplication()
+        app.launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+
+        app.terminate()
+        app = nil
+    }
+    
+    func testAddRecipe() {
+        // wait for the sample recipe to be visible (in a fragile way :))
+        XCTAssert(app.staticTexts["Created 1/28/2024, 2:08 PM"].waitForExistence(timeout: 5))
+        
+        // tap on the add button
+        app.buttons["Add Recipe"].tap()
+        
+        // make sure the add recipe screen is shown
+        XCTAssert(app.staticTexts["Add Recipe"].waitForExistence(timeout: 5))
+    }
 }
